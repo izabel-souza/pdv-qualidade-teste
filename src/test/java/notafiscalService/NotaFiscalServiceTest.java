0f7841e78bfa466d4b713326b6114335b2113d19
Izabel Soares <102875884+izabel-souza@users.noreply.github.com>
2025-10-04 19:55:33 -0300

    ajustes

--- src/test/java/notafiscalService/NotaFiscalServiceTest.java
+++ src/test/java/notafiscalService/NotaFiscalServiceTest.java
@@ -37,15 +37,6 @@ class NotaFiscalServiceTest {
         //objeto da classe NotaFiscalService
         NotaFiscalService nfeService = new NotaFiscalService();
 
-        /*
-        como notasFiscais (NotaFiscalRepository) é privado, não é possível rodar: nfeService.notasFiscais = mockRepo;
-        porque ao instanciar NotaFiscalService manualmente, os atributos private tem valor nulo.
-   
-   		O ReflectionTestUtils permite setar valores em atributos privados de uma classe.
-   		
-   		Pega o atributo notasFiscais do objeto nfeService e seta o seu valor como mockNFeRepo (NotaFiscalRepository) 
-         */
-        
         //injeta o mock no campo private notasFiscais
         ReflectionTestUtils.setField(nfeService, "notasFiscais", mockNFeRepo);
 
@@ -54,14 +45,13 @@ class NotaFiscalServiceTest {
         NotaFiscal nf2 = new NotaFiscal();
         List<NotaFiscal> listaEsperada = Arrays.asList(nf1, nf2);
 
-        //definição do comportamento do mock -- quando chamar o método findAll() de mockNFeRepo, retorna listaEsperada
+        //definição do comportamento do mock -- quando chamar o método findAll() de mockNFeRepo
         when(mockNFeRepo.findAll()).thenReturn(listaEsperada);
 
         //chama o método lista() de NotaFiscalService
         List<NotaFiscal> resultado = nfeService.lista();
 
-        //verificações de resultados do teste
-        assertEquals(2, resultado.size()); //"Retorna 2 notas fiscais (nf1 e nf2)
+        assertEquals(2, resultado.size()); //Retorna 2 notas fiscais (nf1 e nf2)
         assertEquals(listaEsperada, resultado); //A lista retornada deve ser igual à esperada
     }
 
@@ -75,27 +65,17 @@ class NotaFiscalServiceTest {
         
         //objeto da classe NotaFiscalService
         NotaFiscalService nfeService = new NotaFiscalService();
-
-        /*
-        como notasFiscais (NotaFiscalRepository) é privado, não é possível rodar: nfeService.notasFiscais = mockRepo;
-        porque ao instanciar NotaFiscalService manualmente, os atributos private tem valor nulo.
-   
-   		O ReflectionTestUtils permite setar valores em atributos privados de uma classe.
-   		
-   		Pega o atributo notasFiscais do objeto nfeService e seta o seu valor como mockNFeRepo (NotaFiscalRepository) 
-         */
         
         //injeta o mock no campo private notasFiscais
         ReflectionTestUtils.setField(nfeService, "notasFiscais", mockNFeRepo);
 
-       //definição do comportamento do mock -- quando chamar o método totalNotaFiscalEmitidas() de mockNFeRepo, retorna 5
+       //definição do comportamento do mock -- quando chamar o método totalNotaFiscalEmitidas() de mockNFeRepo
         when(mockNFeRepo.totalNotaFiscalEmitidas()).thenReturn(5);
 
         //chama o método totalNotaFiscalEmitidas() de NotaFiscalService
         int total = nfeService.totalNotaFiscalEmitidas();
 
-        //verificações de resultados do teste
-        assertEquals(5, total); //"O total de notas emitidas deve ser 5
+        assertEquals(5, total); //total de notas emitidas deve ser 5
     }
     
 
@@ -109,15 +89,6 @@ class NotaFiscalServiceTest {
         //objeto da classe NotaFiscalService
         NotaFiscalService nfeService = new NotaFiscalService();
 
-        /*
-        como notasFiscais (NotaFiscalRepository) é privado, não é possível rodar: nfeService.notasFiscais = mockRepo;
-        porque ao instanciar NotaFiscalService manualmente, os atributos private tem valor nulo.
-   
-   		O ReflectionTestUtils permite setar valores em atributos privados de uma classe.
-   		
-   		Pega o atributo notasFiscais do objeto nfeService e seta o seu valor como mockNFeRepo (NotaFiscalRepository) 
-         */
-        
         //injeta o mock no campo private notasFiscais
         ReflectionTestUtils.setField(nfeService, "notasFiscais", mockNFeRepo);
 
@@ -125,13 +96,12 @@ class NotaFiscalServiceTest {
         NotaFiscal nf = new NotaFiscal();
         nf.setCodigo(123L);
         
-        //definição do comportamento do mock -- quando chamar o método busca(Long codnota) de mockNFeRepo, retorna nota fiscal de codigo 123L
+        //definição do comportamento do mock -- quando chamar o método busca(Long codnota) de mockNFeRepo
         when(mockNFeRepo.findById(123L)).thenReturn(Optional.of(nf));
 
         //chama o método busca(Long codnota) de NotaFiscalService
         Optional<NotaFiscal> resultado = nfeService.busca(123L);
 
-        //verificações de resultados do teste
         assertTrue(resultado.isPresent()); //A nota deve existir (estar presente)
         assertEquals(123L, resultado.get().getCodigo()); //verifica se o codigo retornado da nota é 123L
     }
@@ -145,21 +115,8 @@ class NotaFiscalServiceTest {
         NotaFiscalService service = new NotaFiscalService();
 
         //gera nota fiscal com código "1234567890"
-        /*
-         i=0 → último dígito = 0 × peso 2 = 0 → total=0 → peso=3
-         i=1 → 9 × peso 3 = 27 → total=27 → peso=4
-         i=2 → 8 × peso 4 = 32 → total=59 → peso=5
-         i=3 → 7 × peso 5 = 35 → total=94 → peso=6
-         i=4 → 6 × peso 6 = 36 → total=130 → peso=7
-         i=5 → 5 × peso 7 = 35 → total=165 → peso=8
-		 i=6 → 4 × peso 8 = 32 → total=197 → peso=9
-		 i=7 → 3 × peso 9 = 27 → total=224 → peso=10 → reset peso=2
-		 i=8 → 2 × peso 2 = 4 → total=228 → peso=3
-		 i=9 → 1 × peso 3 = 3 → total=231 → peso=4
-         */
         int dv = service.geraDV("1234567890");
 
-        //verificação de resultado do teste
         assertTrue(dv >= 0 && dv <= 9); //retorna true porque dv é 0
     }
     
@@ -178,11 +135,11 @@ class NotaFiscalServiceTest {
         //chama o método salvaXML(String xml, String chaveNfe) de NotaFiscalService
         service.salvaXML(xml, chave);
 
-        // verifica se o arquivo foi criado
+        //verifica se o arquivo foi criado
         File file = new File(new File(".").getCanonicalPath() + "/src/main/resources/xmlNfe/" + chave + ".xml");
         
         //verificação de resultado do teste
-        assertTrue(file.exists(), "O arquivo XML deveria ter sido criado");
+        assertTrue(file.exists());
 
         //deleta arquivo
         file.delete();
@@ -211,11 +168,10 @@ class NotaFiscalServiceTest {
         //verifica se o arquivo existe
         assertTrue(file.exists());
 
-        // chama método para deletar o arquivo
+        //chama método para deletar o arquivo
         service.removeXml(chave);
 
-        // verifica se foi removido
-        assertFalse(file.exists()); //arquivo removido
+        assertFalse(file.exists());
     }
     
     
@@ -230,22 +186,8 @@ class NotaFiscalServiceTest {
         NotaFiscalTotaisServer mockTotaisService = mock(NotaFiscalTotaisServer.class);
 
         // cria service e injeta mocks (campo privado → ReflectionTestUtils aqui é só utilidade do Spring,
-        // mas se não puder usar, pode deixar a ideia no comentário ou imaginar que seria feito pelo @InjectMocks)
         NotaFiscalService nfeService = new NotaFiscalService();
         
-        /*
-        NotaFiscalRepository notasFiscais 
-        EmpresaService empresas
-        NotaFiscalTotaisServer notaTotais
-        PessoaService pessoas
-        
-        são privados, não é possível rodar: nfeService.notasFiscais = mockRepo;
-        porque ao instanciar NotaFiscalService manualmente, os atributos private tem valor nulo.
-   
-   		O ReflectionTestUtils permite setar valores em atributos privados de uma classe.
-   		
-   		Pega o atributo notasFiscais do objeto nfeService e seta o seu valor como mockNFeRepo (NotaFiscalRepository) 
-         */
         ReflectionTestUtils.setField(nfeService, "notasFiscais", mockRepo);
         ReflectionTestUtils.setField(nfeService, "empresas", mockEmpresaService);
         ReflectionTestUtils.setField(nfeService, "pessoas", mockPessoaService);
@@ -254,7 +196,7 @@ class NotaFiscalServiceTest {
         //dados testes necessários
         EmpresaParametro parametro = new EmpresaParametro();
         parametro.setSerie_nfe(1);
-        parametro.setAmbiente(1);
+        parametro.setAmbiente(1);    
 
         Empresa empresa = new Empresa();
         empresa.setParametro(parametro);
@@ -276,7 +218,6 @@ class NotaFiscalServiceTest {
         //chama método para cadastrar a nota fiscal
         String codigoGerado = nfeService.cadastrar(10L, "Venda de produtos", NotaFiscalTipo.SAIDA);
 
-        // validação com asserts simples (JUnit)
         assertEquals("999", codigoGerado);
     }
 
